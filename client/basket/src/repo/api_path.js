a4154d2b1973c6139a1dd833a4ee4910cd3cb1ee
Santhi Ranimekala <santhiranimekala025@gmail.com>
2026-06-07 21:29:45 +0530

    ready to deploy

--- client/basket/src/repo/api_path.js
+++ client/basket/src/repo/api_path.js
@@ -1,7 +1,7 @@
 
 
-export const productUrl="http://localhost:3000/api"
-export const imageUrl="http://localhost:3000"
-export const adminUrl="http://localhost:3000/admin"
-export const emailUrl="http://localhost:3000/email"
-export const cartUrl="http://localhost:3000/cart"
+export const productUrl="https://freshbasket-u5ez.onrender.com/api"
+export const imageUrl="https://freshbasket-u5ez.onrender.com"
+export const adminUrl="https://freshbasket-u5ez.onrender.com/admin"
+export const emailUrl="https://freshbasket-u5ez.onrender.com/email"
+export const cartUrl="https://freshbasket-u5ez.onrender.com/cart"
