36d0c2995ebfac2804014a2018e1b80b3af4d9e3
Santhi Ranimekala <santhiranimekala025@gmail.com>
2026-06-07 14:32:04 +0530

    third commit

--- backend/email/send-otp.js
+++ backend/email/send-otp.js
@@ -4,10 +4,17 @@ const dotEnv = require("dotenv")
 
 dotEnv.config()
 
+// const transporter = nodemailer.createTransport({
+//   host: "smtp.gmail.com",
+//   port: 587,
+//   secure: false, // Use true for port 465, false for port 587
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASSWORD,
+//   },
+// });
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false, // Use true for port 465, false for port 587
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD,
